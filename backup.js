5d17a42391679b2d50337d863e0864d62cfc6f44
MichaelPStimpson <michaelpstimpson@outlook.com>
2021-03-04 11:12:27 -0600

    j

---
+++ backup.js
@@ -0,0 +1,53 @@
+const core = require("@actions/core");
+const glob = require("@actions/glob");
+const fs = require("fs");
+const path = require("path");
+
+var walk = function(dir, done) {
+  var results = [];
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+    var i = 0;
+    (function next() {
+      var file = list[i++];
+      if (!file) return done(null, results);
+      file = path.resolve(dir, file);
+      fs.stat(file, function(err, stat) {
+        //console.log(JSON.stringify(stat));
+        if (stat && stat.isDirectory()) {
+          walk(file, function(err, res) {
+            results = results.concat(res);
+            next();
+          });
+        } else {
+          //console.log("file " + file);
+          if (
+            !file.includes(".nvm") &&
+            !file.includes(".rustup") &&
+            !file.includes(".cargo") &&
+            !file.includes(".cargo") &&
+            !file.includes(".cargo") &&
+            !file.includes(".cargo") &&
+            !file.includes(".cargo")
+          )
+            results.push(file);
+          next();
+        }
+      });
+    })();
+  });
+};
+
+// most @actions toolkit packages have async methods
+async function run() {
+  try {
+    walk(process.env.HOME, function(err, results) {
+      if (err) throw err;
+      console.log(results);
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
--- index.js
+++ index.js
@@ -3,54 +3,22 @@ const glob = require("@actions/glob");
 const fs = require("fs");
 const path = require("path");
 
-var walk = function(dir, done) {
-  var results = [];
-  fs.readdir(dir, function(err, list) {
-    if (err) return done(err);
-    var i = 0;
-    (function next() {
-      var file = list[i++];
-      if (!file) return done(null, results);
-      file = path.resolve(dir, file);
-      fs.stat(file, function(err, stat) {
-        //console.log(JSON.stringify(stat));
-        if (stat && stat.isDirectory()) {
-          walk(file, function(err, res) {
-            results = results.concat(res);
-            next();
-          });
-        } else {
-          //console.log("file " + file);
-          if (
-            !file.includes(".nvm") &&
-            !file.includes(".rustup") &&
-            !file.includes(".cargo") &&
-            !file.includes(".config") &&
-            !file.includes("2.277.1") &&
-            !file.includes(".ghcup") &&
-            !file.includes("perflog") &&
-            !file.includes("_actions") &&
-            !file.includes("_temp") &&
-            !file.includes("warmup") &&
-            !file.includes("perflog")
-          )
-            results.push(file);
-          next();
-        }
-      });
-    })();
-  });
-};
-
 // most @actions toolkit packages have async methods
 async function run() {
   const ms = core.getInput("milliseconds");
   console.log(ms);
   try {
-    walk(process.env.HOME, function(err, results) {
-      if (err) throw err;
-      console.log(results);
-    });
+    fs.readFile(
+      ms + "/sfdx-source/module/main/objects/Account/Account.object-meta.xml",
+      "utf8",
+      (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(data);
+      }
+    );
   } catch (error) {
     core.setFailed(error.message);
   }
